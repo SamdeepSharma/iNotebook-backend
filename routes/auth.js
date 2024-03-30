54de067a548858051cea3b4dcb25d823fe047230
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-03-30 18:06:59 +0530

    added login, getdata endpoints, integrated middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,11 @@ const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const fetchuser = require('../middleware/fetchuser')
 
 JWT_SECRET = 'Samdeep#isa$good$buoy'
 
-//creating new user by post method, doesn't require auth or login
+//#ROUTE:1  creating new user by post method, doesn't require login
 router.post('/createuser',
      [body('name', 'Enter a valid name').isLength({ min: 3 }),
      body('email', 'Enter a valid email').isEmail(),
@@ -26,7 +27,7 @@ router.post('/createuser',
                     res.status(400).json({ error: "Sorry, a user with this email already exists" })
                }
 
-               const salt= await bcrypt.genSalt(10)
+               const salt = await bcrypt.genSalt(10)
                const secPass = await bcrypt.hash(req.body.password, salt)
                user = await User.create({
                     name: req.body.name,
@@ -34,16 +35,69 @@ router.post('/createuser',
                     password: secPass
                })
 
-               const data={
-                    user:{
+               const data = {
+                    user: {
                          id: user.id
                     }
                }
                const authtoken = jwt.sign(data, JWT_SECRET)
-               res.json({"authtoken" : authtoken})
+               res.json({ "authtoken": authtoken })
           } catch (error) {
                console.log(error.message)
                // res.status(500).send("Something seems broken!")
           }
      })
+
+//ROUTE: 2  authenticate a user using api/auth/login and creating login endpoint, no login required
+router.post('/login', [
+     body('email', 'Enter a valid email').isEmail(),
+     body('password', 'Password must be atleast 8 characters').exists()
+], async (req, res) => {
+     const result = validationResult(req);
+     if (!result.isEmpty()) {
+          return res.json({ errors: result.array() });
+     }
+     const {email, password} = req.body;
+     try {
+
+          //check if user with the same email exists already
+          let user = await User.findOne({ email: req.body.email })
+          if (!user) {
+               res.status(400).json({ error: "Email and Password combination does not match" })
+          }
+
+          const passwordCompare = await bcrypt.compare(password, user.password)
+          if(!passwordCompare)
+          {
+               return res.status(400).json({error: "Email and Password combination does not match"})
+          }
+
+          const data = {
+               user: {
+                    id: user.id
+               }
+          }
+          const authtoken = jwt.sign(data, JWT_SECRET)
+          res.json({ "authtoken": authtoken })
+     } catch (error) {
+          console.log(error.message)
+          // res.status(500).send("Opps! Something seems broken.")
+     }
+})
+
+//ROUTE: 3  To get logged in user's details using api/auth/getdata, login required
+router.post('/getdata', fetchuser, async (req,res)=>{
+     try{
+          userId = req.user.id
+          const user = await User.findById(userId).select("-password")
+          res.send(user)
+     }
+     catch(error)
+     {
+          console.log(error.message)
+          res.status(500).send("Internal Server Error")
+     }
+})
+
+
 module.exports = router
