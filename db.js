3f45529ab6da282b0496f317b47b2921882f39ea
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-04-16 16:53:45 +0530

    changed db to atlas (on cloud)

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/iNotebook"
+const uri = "mongodb+srv://SamdeepSharma:Samdeep%40iNotebook@inotebook.7082vbv.mongodb.net/iNotebook";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const connectToDB = async() =>{
-     await mongoose.connect(mongoURI)
-     console.log('Connected to DB Successfully')
+     await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }
 
 module.exports = connectToDB;
