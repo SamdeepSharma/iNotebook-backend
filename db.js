77d72bd6a6856e2b2834615052e5b43eaef18b6d
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-04-18 19:57:57 +0530

    env vars

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
-require("dotenv").config();
+require("dotenv").config()
 
-const password = process.env.MONGO_PASSWORD;
+const password = process.env.MONGO_PASSWORD
 const uri = `mongodb+srv://SamdeepSharma:${password}@inotebook.7082vbv.mongodb.net/iNotebook`;
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const connectToDB = async() =>{
+    console.log(process.env.MONGO_PASSWORD)
      await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
